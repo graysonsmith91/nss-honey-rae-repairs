f8c46c2fdf1ac8a9fa2d633c8cb6d956e6ffbca5
Grayson Smith <graysonsmith91@gmail.com>
2023-01-28 18:52:56 -0600

    Added profile tab and profile edit functionality

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -92,7 +92,7 @@ export const TicketList = ({ searchTermState }) => {
             {
                 filteredTickets.map(
                     (ticket) => {
-                        return <section className="ticket">
+                        return <section className="ticket" key={ticket.id}>
                             <header>{ticket.description}</header>
                             <footer>Emergency: {ticket.emergency ? "🔥" : "No"}</footer>
                         </section>
